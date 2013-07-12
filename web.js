b0b44b6b5bf0281fcda95ab00e3f9cd7262b06a3
Simon Soriano <simon0191@gmail.com>
2013-07-12 01:58:52 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2');
+  var helloBuff = fs.readFile('./index.html',function(err,data) {
+      if(err) throw err;
+      console.log(data);
+  });
+  var hello = helloBuff.toString();
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
